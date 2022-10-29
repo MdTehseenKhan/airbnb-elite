df22b900bf47a66fab93d5d091f15d00ca825b85
Tehseen <47675253+MdTehseenKhan@users.noreply.github.com>
2022-10-29 14:07:34 +0000

    .

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -22,10 +22,10 @@ const Footer = () => {
 	<div className="pb-5 px-5 text-gray-500 flex justify-between">
 		<p>
 			© 2022 Airbnb elite —&nbsp;
-			<a href="https://twitter.com/m_tehseen_khan" target="_blank">@tehseenkhan</a>
+			<a href="https://twitter.com/m_tehseen_khan" target="_blank" rel="noreferrer">@tehseenkhan</a>
 		</p>
 		<span className="inline-flex space-x-3">
-			<a href="https://www.facebook.com/muhammadtehseen.khan.986" target="_blank">
+			<a href="https://www.facebook.com/muhammadtehseen.khan.986" target="_blank" rel="noreferrer">
 				<svg
 					fill="currentColor"
 					strokeLinecap="round"
@@ -37,7 +37,7 @@ const Footer = () => {
 					<path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
 				</svg>
 			</a>
-			<a href="https://twitter.com/m_tehseen_khan" target="_blank">
+			<a href="https://twitter.com/m_tehseen_khan" target="_blank" rel="noreferrer">
 				<svg
 					fill="currentColor"
 					strokeLinecap="round"
@@ -49,7 +49,7 @@ const Footer = () => {
 					<path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
 				</svg>
 			</a>
-			<a href="https://www.instagram.com/ig_haiwaan_not0p/" target="_blank">
+			<a href="https://www.instagram.com/ig_haiwaan_not0p/" target="_blank" rel="noreferrer">
 				<svg
 					fill="none"
 					stroke="currentColor"
@@ -63,7 +63,7 @@ const Footer = () => {
 					<path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
 				</svg>
 			</a>
-			<a href="https://linkedin.com/in/muhammad-tehseen-khan-159825172" target="_blank">
+			<a href="https://linkedin.com/in/muhammad-tehseen-khan-159825172" target="_blank" rel="noreferrer">
 				<svg
 					fill="currentColor"
 					stroke="currentColor"
